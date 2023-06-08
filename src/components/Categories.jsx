54ea6d69cad2267007134b814b1450dbb1b14cf2
DavOlufuwa <davolufuwa@gmail.com>
2023-06-08 12:34:42 +0100

    rendered the category items

---
+++ src/components/Categories.jsx
@@ -0,0 +1,19 @@
+import CategoryItem from './CategoryItem'
+import categories from '../resources/categories.json'
+
+  
+
+
+const Categories = () => {
+  return (
+    <div className='categories-container'>
+      {
+        categories.map((category)=> (
+          <CategoryItem key={category.id} {...category}/>
+        ))
+      }
+    </div>
+  )
+}
+
+export default Categories
---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import '../styles/categories.styles.scss'
+
+const CategoryItem = ({title, imageUrl}) => {
+  return (
+    <div className="category-container">
+    <div className="background-image" style={{backgroundImage: `url(${imageUrl})`}}>
+    </div>
+    <div className="body">
+      <h2>{title}</h2>
+      <p>Shop Now</p>
+    </div>
+  </div>
+  )
+}
+
+export default CategoryItem
