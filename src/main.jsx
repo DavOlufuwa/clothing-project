adbc0ecf39c93e061eea5f410da38f58da778a25
Dav.O <davolufuwa@gmail.com>
2023-06-17 15:45:31 +0100

    optimized main

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { UserProvider } from './context/UserContext.jsx'
 import { CartProvider } from './context/cartContext.jsx'
 import { CategoriesProvider } from './context/categoriesContext.jsx'
 import { FavoritesProvider } from './context/favoritesContext.jsx'
+import { UserProvider } from './context/UserContext.jsx'
 import './index.scss'
 
+UserProvider
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
